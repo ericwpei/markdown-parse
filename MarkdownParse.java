c1c1fb830aaf59750b19f1484693cda21bb86c27
ericwpei <97645828+ericwpei@users.noreply.github.com>
2022-03-02 18:15:38 -0800

    updating main method to account for directories

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,8 +1,12 @@
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
@@ -55,13 +59,33 @@ public class MarkdownParse {
             currentIndex = closeParen + 1;
             // System.out.println("Value of current index after loop: " + currentIndex);
         }
-        System.out.println("test change");
         return toReturn;
     }
+
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
+		/*Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        System.out.println(links);*/
+        System.out.println(getLinks(new File(args[0])));
     }
 }
